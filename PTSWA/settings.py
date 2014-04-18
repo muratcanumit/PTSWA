e9965fcfd83377d72b601852b023cddedc3b5c4a
murat <murat@murat-K55VJ.(none)>
2014-04-18 18:00:37 +0300

    updated settings.py

--- PTSWA/settings.py
+++ PTSWA/settings.py
@@ -12,8 +12,8 @@ MANAGERS = ADMINS
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'muratcanumit@gmail.com'
-EMAIL_HOST_PASSWORD = '20090105007Mc'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 DATABASES = {
