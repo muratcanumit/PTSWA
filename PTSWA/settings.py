d56b03323fc1f7952ae160c81bce034524c08e75
murat <murat@murat-K55VJ.(none)>
2014-04-25 13:01:24 +0300

    error 404, 500 pages, templates, satic files

--- PTSWA/settings.py
+++ PTSWA/settings.py
@@ -61,17 +61,17 @@ MEDIA_ROOT = ''
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/murat/Envs/GRADPROJECT/PTSWA/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/home/murat/Envs/GRADPROJECT/PTSWA/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -130,7 +130,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # 'django.contrib.admindocs',
     'device',
-    'debug_toolbar',
+    # 'debug_toolbar',
 )
 
 # A sample logging configuration. The only tangible logging
--- PTSWA/urls.py
+++ PTSWA/urls.py
@@ -1,5 +1,7 @@
+# from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 admin.autodiscover()
 
 urlpatterns = patterns(
@@ -10,7 +12,20 @@ urlpatterns = patterns(
         name="status"),
 )
 
+# if settings.DEBUG is True:
+#    urlpatterns = patterns(
+#        '',
+#        url(r'^static/(?P<path>.*)$',
+#            'django.views.static.serve',
+#            {'document_root': settings.STATIC_ROOT}),
+#    ) + urlpatterns
+
+# urlpatterns += staticfiles_urlpatterns()
+
 urlpatterns += patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
 )
+
+handler404 = 'PTSWA.views.handler404'
+handler500 = 'mysite.views.handler500'
